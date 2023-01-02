67e1b04441181f9cc044e3b2500fd8d0199f3200
ByeongJunAn <abj5237@nate.com>
2023-01-02 13:28:25 +0900

    '2023-01-02'

---
+++ spro01/src/main/java/com/keduit/sample/Chef.java
@@ -0,0 +1,11 @@
+package com.keduit.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+public class Chef {
+
+}
---
+++ spro01/src/main/java/com/keduit/sample/Restaurant.java
@@ -0,0 +1,16 @@
+package com.keduit.sample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+import lombok.Setter;
+
+@Component
+@Data
+public class Restaurant {
+	
+	@Setter(onMethod_ = @Autowired)
+	// onMethod=@_({AnnotationGHoHere})) 자바 1.8 이전 표현법
+	private Chef chef;
+}
